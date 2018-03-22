6f88e9367f267076e04058e97f91cab9f1db106d
Federico Miras <miras.federico@gmail.com>
2018-03-22 15:39:11 -0300

    Support hotel querying

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 require('dotenv').config()
+const { parse } = require('url')
 const { MongoClient } = require('mongodb')
+const { router, get } = require('microrouter')
 
 const { MONGO_URL, MONGO_DB } = process.env
 
-const getHotels = async () => {
+const queryForHotels = async query => {
   const connection = await MongoClient.connect(MONGO_URL)
   const db = connection.db(MONGO_DB)
   const collection = db.collection('hotels')
-  const jobs = await collection.find().toArray()
+  const jobs = await collection.find(query).toArray()
   connection.close()
   return jobs
 }
 
 module.exports = async (req, res) => {
-  const hotels = await getHotels()
+  const query = parse(req.url, true).query
+  const hotels = await queryForHotels(query)
   hotels.map(hotel => delete hotel._id)
   return { hotels }
 }
